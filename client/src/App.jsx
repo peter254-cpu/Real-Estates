b3ebe31b3d07d6e59a335639c25a1cc2b9fda1cc
Peter <peterslap67@gmail.com>
2024-12-30 20:39:00 +0300

    Done with the real esates app

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Headers from "./components/Headers";
+import SignUp from "./pages/SignUp";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing"
+import Search from "./components/Search";
+import React from "react";
+
+
+
+
+export default function App() {
+  return <BrowserRouter>
+  <Headers />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="sign-in" element = {<SignIn />} />
+      <Route path="/sign-up" element ={<SignUp />} />
+      <Route path="/about" element ={<About />} />
+      <Route path="/listing/:listingId" element={<Listing />} />
+      <Route path="/search" element={<Search />} />
+      <Route element={<ProtectedRoutes />}>
+          <Route path="/profile" element ={<Profile />} />
+          <Route path="/create-listing" element ={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element ={<UpdateListing />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+}
---
+++ client/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  root: './client',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',  // Ensure this path is correct relative to the 'root' directory
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""), 
+      },
+    },
+  },
+});
+
+
