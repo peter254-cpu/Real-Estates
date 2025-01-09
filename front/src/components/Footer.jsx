d46a9d191aea3d5231ffd008e54572bf693660fc
Peter <peterslap67@gmail.com>
2025-01-09 17:15:46 +0300

    added input box

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
     const { currentUser } = useSelector((state) => state.user)
   return (
     <div className="bg-gray-800 text-gray-200 pr-16 pl-16 mt-8 pt-8 pb-8 flex justify-between">
-        <div>
+        <div className="flex-1">
             <h2>RealestEstates</h2>
             <p>At RealestEstates we provide a pool to decent homes at your locality within your budget </p>
         </div>
-        <div className="flex flex-col items-start ">
+        <div className="flex flex-col items-start flex-1">
             <h3>SiteMap</h3>
                 <Link>
                     Home
@@ -29,8 +29,32 @@ const Footer = () => {
                 </>
             )}
         </div>
-        <div>
-            <h3>Subscribe to get updates of listings near you</h3>
+        <div className="flex-1">
+        <div class="flex items-center justify-center h-screen">
+            <div class="w-full max-w-sm">
+                <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    Sign up for Property Listings Updates
+                    </label>
+                    <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    />
+                </div>
+                <div class="flex items-center justify-between">
+                    <button
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit"
+                    >
+                    Sign Up
+                    </button>
+                </div>
+                </form>
+            </div>
+            </div>
         </div>
     </div>
   )
